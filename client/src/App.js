2763b704ee8bb7d06c7658be422f7d133c452c6a
Ian Herrera <iantherrera@gmail.com>
2022-11-17 02:31:28 -0500

    Add list view feature Add reset list feature Edit styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Route, Routes } from "react-router-dom";
 
 import MovieCards from './components/movieCard';
+
 
 
 function App() {
   // App render
   return (
     <>
-      <div id="bgWrap"></div>
-      <header>
-        <div id="pageTitleContainer" className='bubbleFrame'>
-          <h1 id="pageTitle">cinefile</h1>
-        </div>
-        <nav id="nav">
-          <ul id="navList">
-            <li className='bubbleFrame'>listview</li>
-            <li className='bubbleFrame'>LOGOUT</li>
-          </ul>
-        </nav>
-      </header>
       <Routes>
         <Route exact path="/" element={<MovieCards />} />
       </Routes>
--- client/src/components/movieCard.js
+++ client/src/components/movieCard.js
@@ -20,8 +20,9 @@ const MovieCard = (props) => (
   <div className="movieCard">
     <div className='cardOptionsContainer'>
       <div className='viewedCheckContainer'>
-        <label>
+        <label className="checkLabel">
           <input
+            className="checkbox"
             type="checkbox"
             defaultChecked={props.metaData.checked}
             onChange={() => {
@@ -31,9 +32,10 @@ const MovieCard = (props) => (
           Viewed
         </label>
       </div>
-      <div className='favoriteButton'>
-        <label>
+      <div className='favoriteCheckContainer'>
+        <label className="checkLabel">
           <input
+            className="checkbox"
             type="checkbox"
             defaultChecked={props.metaData.favorite}
             onChange={() => {
@@ -44,27 +46,26 @@ const MovieCard = (props) => (
         </label>
       </div>
       <div className='deleteCardContainer'>
-        <button className="button-link" type="button"
+        <button className="deleteButton" type="button"
           onClick={() => {
             props.deleteMovie(props.metaData.id);
-            console.log(props.metaData.id)
           }}
         >
           Delete
         </button>
       </div>
     </div>
-    <div className='rankTitleYearContainer'>
-      <div className="movieRank">{props.metaData.rank}</div>
-      <div className='movieTitleYearContainer'>
-        <div className='movieTitle'>{props.metaData.movieTitle}</div>
-        <div className='movieYear'>{props.metaData.year}</div>
+    <div className='movieInfoContainer'>
+      <div className="rank">{props.metaData.rank}</div>
+      <div className='titleYearContainer'>
+        <div className='title'>{props.metaData.movieTitle}</div>
+        <div className='year'>{props.metaData.year}</div>
       </div>
     </div>
     <div className='moviePosterContainer'>
       <img src={`https://image.tmdb.org/t/p/original/${props.metaData.posterPath}`} alt="Movie poster" className='posterImg' />
     </div>
-    <div className='movieSynopsis'>
+    <div className='synopsis'>
       {props.metaData.overview}
     </div>
   </div>
@@ -74,12 +75,12 @@ const MovieCard = (props) => (
 export default function MovieCards() {
   const [userData, setUserData] = useState([]);
   const [movieData, setMovieData] = useState([]);
-
-  /*   const initMovieData = require("../initData/initMovieData.json"); */
+  const [isListView, setIsListView] = useState(false);
+  const initMovieData = require("../initData/initMovieData.json");
 
   const userId = '6369d823852305533b133048';
 
-  // Method to fetch user data from database
+  // Method to fetch user data and render cards when movie list updates
   useEffect(() => {
     async function getUserData() {
       const response = await fetch(`http://localhost:5000/userData/${userId}`);
@@ -94,7 +95,6 @@ export default function MovieCards() {
       setUserData(userData);
       const movieData = userData.movieData;
       setMovieData(movieData);
-      console.log(userData);
     }
 
     getUserData();
@@ -102,16 +102,9 @@ export default function MovieCards() {
     return;
   }, [movieData.length]);
 
-  // Initialize movie list for new users
-  /*   if (typeof userData.movieData === "undefined" || userData.movieData.length === 0) {
-      userData.movieData = initMovieData;
-      updateUserDB(userData._id);
-    } */
-
   // Method to update user data in server
-  // This will send a post request to update the data in the database.
+  // Post request to update the data in the database
   async function updateUserDB(id) {
-    console.log(userData);
     const updatedUser = userData;
 
     await fetch(`http://localhost:5000/update/${id}`, {
@@ -121,12 +114,10 @@ export default function MovieCards() {
         'Content-Type': 'application/json'
       },
     });
-
   }
 
-  // TODO Method to toggle viewed status and update user data
+  // Toggle viewed status and update user data
   async function toggleViewed(id) {
-    console.log(id);
     const checked = userData.movieData.find(movie => movie.id === id).checked;
     const toggleCheck = !checked;
     userData.movieData.find(movie => movie.id === id).checked = toggleCheck;
@@ -134,9 +125,8 @@ export default function MovieCards() {
     await updateUserDB(userData._id);
   }
 
-  // TODO Method to toggle favorite and update user data
+  // Toggle favorite and update user data
   async function toggleFavorite(id) {
-    console.log(id);
     const favorite = userData.movieData.find(movie => movie.id === id).favorite;
     const toggleFavorite = !favorite;
     userData.movieData.find(movie => movie.id === id).favorite = toggleFavorite;
@@ -144,7 +134,7 @@ export default function MovieCards() {
     await updateUserDB(userData._id);
   }
 
-  // This method will delete a movie entry
+  // Delete a movie entry
   async function deleteMovie(id) {
     const newMovieData = userData.movieData.filter((movies) => movies.id !== id);
     userData.movieData = newMovieData;
@@ -152,12 +142,10 @@ export default function MovieCards() {
 
     setUserData(userData);
     setMovieData(newMovieData);
-    console.log(userData);
   }
 
-  // Method to map out movie data & populate metadata for each movie card
+  // Populate metadata for each movie card
   function movieCards() {
-    console.log(userData.movieData);
     const movieMetaData = movieData;
     return movieMetaData.map((metaData) => {
       metaData.id = metaData.imdbId;
@@ -173,10 +161,75 @@ export default function MovieCards() {
     });
   }
 
-  // Displays the movie cards
+  // View style button
+  function viewStyleButton() {
+    return (
+      <button id="viewButton" className="navButton" type="button"
+        onClick={() => {
+          const currentListView = !isListView;
+          setIsListView(isListView => !isListView);
+          if (currentListView) {
+            const movieListContainers = document.querySelectorAll("#listViewContainer *");
+            for (let i = 0; i < movieListContainers.length; i++) {
+              movieListContainers[i].classList.add("listContainer");
+            }
+          } else {
+            const movieListContainers = document.querySelectorAll("#movieCardsContainer *");
+            for (let i = 0; i < movieListContainers.length; i++) {
+              movieListContainers[i].classList.remove("listContainer");
+            }
+          }
+        }}
+      >
+        {isListView ? "gallery" : "listview"}
+      </button>
+    )
+  }
+
+  // List reset button
+  function resetButton() {
+    return (
+      <button id="resetButton" className="navButton" type="button"
+        onClick={() => {
+          resetList();
+        }}
+      >
+        Reset List
+      </button>
+    )
+  }
+
+  // List reset
+  async function resetList() {
+    userData.movieData = initMovieData;
+    await updateUserDB(userData._id);
+    setUserData(userData);
+    setMovieData(userData.movieData);
+  }
+
+  // Title header and menu bar
+  function header() {
+    return (
+      <header>
+        <div id="siteTitleContainer">
+          <h1 id="siteTitle">cinefile</h1>
+        </div>
+        <nav>
+          {viewStyleButton()}
+          {resetButton()}
+        </nav>
+      </header>
+    )
+  }
+
+  // Renders component display
   return (
-    <div id='movieCardsContainer'>
-      {movieCards()}
+    <div id="page">
+      {header()}
+      <div id="bgWrap"></div>
+      <div id={isListView ? 'listViewContainer' : 'movieCardsContainer'} className="contentContainer">
+        {movieCards()}
+      </div>
     </div>
   );
 }
